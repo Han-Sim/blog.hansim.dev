e7d1f831148a46d1c24134de5e0a963bb850e91e
Han Sim <han.sim.canada@gmail.com>
2019-06-20 01:17:31 -0400

    create and fetch posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,9 +14,31 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-catch-links`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,52 @@
 import React from "react"
 import { Link } from "gatsby"
+import { graphql, StaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Post from "../components/Post"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" /> 
     <h1>Main Page</h1>
+    <StaticQuery query={IndexQuery} render={data => {
+      return (
+        <div>
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+            <Post title={node.frontmatter.title}
+              author={node.frontmatter.author}
+              path={node.frontmatter.path}
+              date={node.frontmatter.date}
+              body={node.excerpt}
+            />          
+          ))}
+        </div>
+      )
+    }}/>
   </Layout>
 )
+
+const IndexQuery = graphql`
+  query {
+    allMarkdownRemark(sort: {
+      fields: [frontmatter___date], 
+      order: DESC
+    }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString:"MMM Do YYYY")
+            author
+            path
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
