e80e639c7a37a63aab6e62f0490a2bc8aacc8116
Han Sim <han.sim.canada@gmail.com>
2019-06-20 01:40:18 -0400

    create thumbnail image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import { graphql, StaticQuery } from "gatsby"
+import { Row, Col } from "reactstrap" 
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,39 +9,61 @@ import Post from "../components/Post"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" /> 
+    <SEO title="Home" />
     <h1>Main Page</h1>
-    <StaticQuery query={IndexQuery} render={data => {
-      return (
-        <div>
-          {data.allMarkdownRemark.edges.map(({ node }) => (
-            <Post title={node.frontmatter.title}
-              author={node.frontmatter.author}
-              path={node.frontmatter.path}
-              date={node.frontmatter.date}
-              body={node.excerpt}
-            />          
-          ))}
-        </div>
-      )
-    }}/>
+    <Row>
+      <Col md="8">
+        <StaticQuery
+          query={IndexQuery}
+          render={data => {
+            return (
+              <div>
+                {data.allMarkdownRemark.edges.map(({ node }) => (
+                  <Post
+                    title={node.frontmatter.title}
+                    author={node.frontmatter.author}
+                    path={node.frontmatter.path}
+                    date={node.frontmatter.date}
+                    body={node.excerpt}
+                    fluid={node.frontmatter.image.childImageSharp.fluid}
+                  />
+                ))}
+              </div>
+            )
+          }}
+        />
+      </Col>
+      <Col md="4">
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0,0,0,0.4)",
+          }}
+        ></div>
+      </Col>
+    </Row>
   </Layout>
 )
 
 const IndexQuery = graphql`
   query {
-    allMarkdownRemark(sort: {
-      fields: [frontmatter___date], 
-      order: DESC
-    }) {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
           id
           frontmatter {
             title
-            date(formatString:"MMM Do YYYY")
+            date(formatString: "MMM Do YYYY")
             author
             path
+            image {
+              childImageSharp {
+                fluid(maxWidth: 600) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
           }
           excerpt
         }
