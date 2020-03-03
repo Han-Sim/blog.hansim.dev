f6a1a6fa785455aeaa449f0030f0c71e89dbe35f
Han Sim <han.sim.dev@gmail.com>
2020-03-02 22:15:13 -0500

    [update category] renamed file.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import _ from "lodash";
 
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import SEO from "../components/seo";
 import Post from "../components/post";
 
