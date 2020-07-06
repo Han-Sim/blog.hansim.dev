e3d2e139a5104c0f5cb201ab12c84a0af06466f6
Han Sim <han.sim.dev@gmail.com>
2020-07-06 17:09:03 -0400

    [renewal] created PostCard

---
+++ src/components/postCard/components/PostCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const PostCard = ({ title, author, slug, date, body, tags }) => {
+  return <div>test</div>;
+};
+
+export default PostCard;
