d61057fa1af428660432205ca16150405709907e
Han Sim <han.sim.dev@gmail.com>
2020-07-25 21:42:14 -0400

    [renewal] updated the category link for each post.

--- src/components/post/components/Post.Header.jsx
+++ src/components/post/components/Post.Header.jsx
@@ -1,17 +1,31 @@
-import React from "react";
-
-import { slugify } from "src/util/helperFunctions";
+import React, { useCallback } from "react";
+import { navigate } from "gatsby";
+import { KEY_ENTER } from "src/util/constants";
 import style from "./post.header.module.scss";
 
 const PostHeader = ({ author, category, date, title }) => {
+  const handleCategoryOnClick = useCallback(() => {
+    navigate(`/all-posts`);
+  }, []);
+
+  const handleCategoryOnKeyDown = useCallback(event => {
+    if (event.keyCode === KEY_ENTER) {
+      navigate(`/all-posts`);
+    }
+  }, []);
+
   return (
     <div className={style.postHeaderContainer}>
       <div>
         <div className={style.info}>
-          <div className={style.category}>
-            <a href={`/category/${slugify(category)}`}>
-              {category.toUpperCase()}
-            </a>
+          <div
+            className={style.category}
+            onClick={handleCategoryOnClick}
+            onKeyDown={handleCategoryOnKeyDown}
+            role="button"
+            tabindex={0}
+          >
+            {category.toUpperCase()}
           </div>
           <div className={style.divider} />
           <div className={style.dateAndAuthor}>
