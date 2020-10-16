3bc98e5fe6783cd5bbaa1d90fb384f854bbd8ae9
Han Sim <han.sim.dev@gmail.com>
2020-10-15 23:32:03 -0400

    [mobile] updated post header.

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,18 +7,7 @@ import SEO from "src/components/seo";
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404 Error" />
-    <div className="post-header-area">
-      <div className="post-header">
-        <div className="post-title">
-          <h1>Something went wrong:/</h1>
-        </div>
-        <div className="post-list text-center">
-          <Link to={"/"}>
-            <h1>Go to the first page</h1>
-          </Link>
-        </div>
-      </div>
-    </div>
+    <div>Something went wrong :/</div>
   </Layout>
 );
 
