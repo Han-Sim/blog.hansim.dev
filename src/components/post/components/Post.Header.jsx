8fe4b3fa7a2233c578a978d2f9ec2c2be906672e
Han Sim <han.sim.dev@gmail.com>
2021-12-26 15:24:00 +0900

    removed category from post header

--- src/components/post/components/Post.Header.jsx
+++ src/components/post/components/Post.Header.jsx
@@ -1,37 +1,13 @@
-import React, { useCallback } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { navigate } from "gatsby";
-import { KEY_ENTER } from "src/util/constants";
 import style from "./post.header.module.scss";
 
-const PostHeader = ({ author, category, date, title }) => {
-  const handleCategoryOnClick = useCallback(() => {
-    navigate(`/posts`);
-  }, []);
-
-  const handleCategoryOnKeyDown = useCallback(event => {
-    if (event.keyCode === KEY_ENTER) {
-      navigate(`/posts`);
-    }
-  }, []);
-
+const PostHeader = ({ author, date, title }) => {
   return (
     <div className={style.postHeaderContainer}>
       <div>
         <div className={style.title}>{title}</div>
         <div className={style.footer}>
-          <div className={style.categoryContainer}>
-            <div className={style.category}>Category</div>
-            <div
-              className={style.categoryName}
-              onClick={handleCategoryOnClick}
-              onKeyDown={handleCategoryOnKeyDown}
-              role="button"
-              tabIndex={0}
-            >
-              {category}
-            </div>
-          </div>
           <div className={style.authorContainer}>
             <div className={style.writtenBy}>Written by</div>
             <div>{author}</div>
@@ -48,7 +24,6 @@ const PostHeader = ({ author, category, date, title }) => {
 
 PostHeader.propTypes = {
   author: PropTypes.string,
-  category: PropTypes.string,
   date: PropTypes.string,
   title: PropTypes.string,
 };
