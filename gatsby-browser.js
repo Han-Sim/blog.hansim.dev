d054e13a4a2e7d398d705660434083c7d4f344fc
Han Sim <han.sim.dev@gmail.com>
2022-01-09 06:47:07 +0900

    refactored prism

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,8 +6,8 @@
 const React = require("react");
 const PropTypes = require("prop-types");
 const { ContextProvider } = require("./src/context");
+require("prismjs/themes/prism.css");
 require("./src/styles/index.scss");
-require("./src/styles/prism.css");
 
 const App = ({ element }) => {
   // Provide Context.
